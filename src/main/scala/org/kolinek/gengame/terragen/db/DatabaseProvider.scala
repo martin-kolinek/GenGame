3a927fbd73fa5193afac07899098e874397fbc62
Martin Kolinek <martin.kolinek@gmail.com>
2014-06-09 19:52:32 +0200

    Creation of the database

--- src/main/scala/org/kolinek/gengame/terragen/db/DatabaseProvider.scala
+++ src/main/scala/org/kolinek/gengame/terragen/db/DatabaseProvider.scala
@@ -12,13 +12,11 @@ trait InMemoryDatabaseProvider extends DatabaseProvider with TerragenTables {
         val db = Database.forName(":memory:")
         db.withTransaction { implicit s =>
             (pointsTable.ddl ++ trianglesTable.ddl ++ doneChunksTable.ddl ++ doneMeshChunksTable.ddl ++ meshesTable.ddl).create
-            /*sqlu"CREATE INDEX ix_points_pos ON points(direction, posx, posy, posz)".execute(s)
-            sqlu"CREATE VIRTUAL TABLE meshbounds USING rtree (meshid int, minx float, maxx float, miny float, maxy float, minz float, maxz float)".execute
-            sqlu"CREATE INDEX ix_trianglecubes ON triangles(cubeid)".execute
-            sqlu"CREATE VIRTUAL TABLE donertree USING rtree_i32 (cubeid int, minx int, maxx int, miny int, maxy int, minz int, maxz int)".execute
-            sqlu"CREATE INDEX ix_donecubes ON donecubes(x, y, z)".execute
-            sqlu"CREATE INDEX ix_meshdonecubes ON meshdonecubes(x, y, z)".execute
-            sqlu"CREATE INDEX ix_meshdef ON meshdef(meshid, sequenceno)".execute*/
+            Q.updateNA("CREATE INDEX ix_points_pos ON points(direction, posx, posy, posz)").execute
+            Q.updateNA("CREATE VIRTUAL TABLE meshbounds USING rtree (meshid int, minx float, maxx float, miny float, maxy float, minz float, maxz float)").execute
+            Q.updateNA("CREATE INDEX ix_trianglecubes ON triangles(cubeid)").execute
+            Q.updateNA("CREATE INDEX ix_donecubes ON donechunks(x, y, z)").execute
+            Q.updateNA("CREATE INDEX ix_meshdonecubes ON donemeshchunks(x, y, z)").execute
         }
         db
     }
